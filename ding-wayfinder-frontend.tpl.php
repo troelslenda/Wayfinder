44237375991fed10bc1190170b58108398b2b8b8
Troels Lenda <troelslenda@gmail.com>
2012-01-13 14:20:11 +0100

    correcting path to icon

--- ding-wayfinder-frontend.tpl.php
+++ ding-wayfinder-frontend.tpl.php
@@ -7,7 +7,7 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <meta name="format-detection" content="telephone=no" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="apple-touch-icon-precomposed" href="theme/icon.png"/>
+    <link rel="apple-touch-icon-precomposed" href="<?php print url(drupal_get_path('module', 'ding_wayfinder').'/images/icon.png'); ?>"/>
     <?php echo $styles; ?>
     <?php echo $scripts; ?>
 
