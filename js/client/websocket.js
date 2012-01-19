d68f8619cca9366e61f5e6f3200399b30c81944f
Troels Lenda <troelslenda@gmail.com>
2012-01-19 10:25:33 +0100

    cleanup

--- js/client/websocket.js
+++ js/client/websocket.js
@@ -1,33 +1,49 @@
 
-var server = '';
-var devicename = localStorage.getItem('devicename');
+var websocket = {
+
+  'server' : '',
+  'init' : function (){
+    server = new WebPush(Drupal.settings.ding_wayfinder.settings.server);
+    this.handlers();
+  },
+  'handlers' : function(){
+    server.bind('open',function(){
+      websocket.connectionstatus('on');
+    });
+    server.bind('connection_disconnected', function() {
+      websocket.connectionstatus('off');
+    });
+    server.bind('close', function() {
+      websocket.connectionstatus('off');
+    });
+    server.bind('connection_failed', function() {
+      websocket.connectionstatus('off');
+    });
+
+    server.bind('message',function(data){
+      message = eval("("+data+")");
+      if((message.recipient == localStorage.getItem('devicename') || message.recipient == 'all')  && message.action == 'hello') {
+        server.send(JSON.stringify(websocket.updatemessage()));
+      }
+      if((message.recipient == localStorage.getItem('devicename') || message.recipient == 'all')  && message.action == 'restart') {
+        window.location.reload();
+      }
+    });
+  },
+  'connectionstatus': function(sta){
+
+  },
+  'updatemessage' : function(){
+    return {
+      'name' : localStorage.getItem('devicename'),
+      'recipient' : 'server',
+      'action' : 'lastupdate',
+      'data' : Drupal.settings.ding_wayfinder.settings.launched,
+    };
+},
+
+};
 
 jQuery(function(){
-
-  server = new WebPush(Drupal.settings.ding_wayfinder.settings.server);
-
-  server.bind('open', function() {
-  });
-  server.bind('connection_disconnected', function() {
-    ll.d('disconnected :/');
-  });
-  server.bind('close', function() {
-  });
-  server.bind('connection_failed', function() {
-  });
-  server.bind('message', function(data) {
-    message = eval("("+data+")");
-    if((message.recipient == devicename || message.recipient == 'all')  && message.action == 'hello') {
-      msg = {};
-      msg.name = devicename;
-      msg.recipient = 'server';
-      msg.action = 'lastupdate';
-      msg.data = Drupal.settings.ding_wayfinder.settings.launched;
-    
-      server.send(JSON.stringify(msg));
-    }
-    if((message.recipient == devicename || message.recipient == 'all')  && message.action == 'restart') {
-      window.location.reload();
-    }
-  });
+  websocket.init();
 });
