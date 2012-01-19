910f0c98fc66c738c1dcb8a475000f29d52780b6
Troels Lenda <troelslenda@gmail.com>
2012-01-19 15:48:50 +0100

    testing scroll

--- js/client/setup.js
+++ js/client/setup.js
@@ -112,10 +112,10 @@ end = e.originalEvent.changedTouches[0];
 $('body .topbar .logo').html('X' + end.screenX + ':' + pos.x+ " \nY" + end.screenY + ':'+pos.y);
 
 
-        if(end.originalEvent.touches[0].pageX != pos.x){
+        if(end.originalEvent.touches[0].screenX != pos.x){
           return;
         }
-        if(end.originalEvent.touches[0].pageY != pos.y){
+        if(end.originalEvent.touches[0].screenY != pos.y){
           return;
         }
 
